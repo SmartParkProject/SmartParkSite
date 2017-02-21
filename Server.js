8cf4b44184c806b1161c78ac05bc7477075979b0
Blackale11 <aely@umflint.edu>
2017-02-21 02:43:14 -0500

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = __dirname + '/views/'
+var path = __dirname + '/views/webpages/'
 
 router.use(function (req, res, next){
 	console.log("/" + req.method);
@@ -10,6 +10,30 @@ router.use(function (req, res, next){
 
 router.get("/",function(req, res){
 	res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req, res){
+	res.sendFile(path + "about.html");
+});
+
+router.get("/login",function(req, res){
+	res.sendFile(path + "login.html");
+});
+
+router.get("/accountman",function(req, res){
+	res.sendFile(path + "accountman.html");
+});
+
+router.get("/CreateAccount",function(req, res){
+	res.sendFile(path + "CreateAccount.html");
+});
+
+router.get("/viewParkingMap",function(req, res){
+	res.sendFile(path + "viewParkingMap.html");
+});
+
+router.get("/viewParkingSpots",function(req, res){
+	res.sendFile(path + "viewParkingSpots.html");
 });
 
 app.use("/",router);
